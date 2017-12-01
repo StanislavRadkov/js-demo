4f15c50a78d1e8e8cfbc49505b4214d8524bd60c
Stanislav Radkov <Stanislav.Radkov@skyscanner.net>
2017-12-01 20:20:47 +0200

    Added something

---
+++ es-6-features/0.use-strict.js
@@ -0,0 +1,6 @@
+'use strict'; // function or global scope
+
+https://www.w3schools.com/js/js_strict.asp
+
+var a = 5;
+del a;
---
+++ weak-set/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const count = 10000;
+
+var arr = [];
+var set = new WeakSet();
+
+$('document').ready(() => {
+    $('#run-with-array').click(() => {
+        for (var i = 0; i < count; i++) {
+            var elem = $('<span class="badge badge-primary">ELEMENT</span>');
+            arr[i] = elem;
+            $('#holder').append(elem);
+            elem.remove();
+        }
+
+        console.log('USING ARRAY: DONE!');        
+    });
+
+    $('#clear-array').click(() => {
+        arr.length = 0;
+    });
+
+    $('#run-with-weakSet').click(() => {
+        for (var i = 0; i < count; i++) {
+            var elem = $('<span class="badge badge-primary">ELEMENT</span>');
+            set.add(elem);
+            $('#holder').append(elem);
+            elem.remove();
+        }
+
+        console.log('USING WEAKSET: DONE!');
+    });
+
+});
