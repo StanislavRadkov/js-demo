c9042efc06e59bc1b03fef2a8ddb61efb3ec35b0
Stanislav Radkov <Stanislav.Radkov@skyscanner.net>
2017-12-04 21:43:24 +0200

    WIP

--- async-server/app.js
+++ async-server/app.js
@@ -5,10 +5,23 @@ var server = express();
 const port = 8080;
 console.log(`Listening on port ${port}.`)
 
-
-server.get('/api/hello', async function(req, res) {
-  var data = await request('http://www.googleqweqwee.com/');
+server.get('/api/hello', async function (req, res) {
+  var data = await request('http://www.google.com/');
   res.send(data);
+});
+
+// Error example
+async function throwError() {
+  throw new Error('Opps!');
+}
+
+server.get('/api/error', async function (req, res) {
+  try {
+    var data = await throwError();
+    res.send(data);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
 server.listen(port);
